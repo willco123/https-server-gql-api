f072027702c31bf21bc17b206b437ce0bd0963d0
willco213 <87068102+willco123@users.noreply.github.com>
2024-03-02 18:48:48 +0000

    add working https

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
 import "dotenv/config";
 import https from "https";
+import http from "http";
 import fs from "fs";
 
 import app from "./app.js";
 
-const hostname = "mysite.example";
+const env = process.env.NODE_ENV;
+
+const hostname = "myApi.example";
 const options = {
-  cert: fs.readFileSync("./mysite.example.pem"),
-  key: fs.readFileSync("./mysite.example-key.pem"),
+  cert: fs.readFileSync("./myApi.example.pem"),
+  key: fs.readFileSync("./myApi.example-key.pem"),
 };
 const port = process.env.PORT;
-https
-  .createServer(options, function (req, res) {
-    app(req, res);
-  })
-  .listen({ port, hostname: hostname }, () => {
-    console.log(`Server is listening on https://${hostname}:${port}`);
-  });
+env === "live"
+  ? https
+      .createServer(options, function (req, res) {
+        app(req, res);
+      })
+      .listen({ port, hostname }, () => {
+        console.log(`Server is listening on https://${hostname}:${port}`);
+      })
+  : http.createServer(app).listen({ port, hostname: hostname }, () => {
+      console.log(`Server is listening on http://${hostname}:${port}`);
+    });
